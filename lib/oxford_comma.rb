d81d5f0871e9ad269527475d3b0d63d14859584a
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-07 10:44:53 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  case array.size
+  when 1
+    p array.join
+  when 2
+    p array.join(" and ")
+  else
+    remainder = array.pop
+    oxford = array.join(", ")
+    oxford << ", and #{remainder}"
+    p oxford
+  end
 end
