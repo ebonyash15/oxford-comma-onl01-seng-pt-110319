e52cdc119d9ea0d51470a2f84fa888f303e28521
Ebony Sharrock <ebonysharrock@gmail.com>
2019-11-07 10:35:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  case array.size
+  when ==1
+    puts array.join
+  when ==2
+    puts array.join(" and ")
+  when >2
+    oxford = array.pop
+    oxford1 = oxford.join(", ")
+    oxford1 << array.last
+    puts oxford1
 end
